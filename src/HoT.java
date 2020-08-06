00425a2ee5f767cb4f14a7329d6c9ff8d435ae53
kyotatanji <69285633+kyotatanji@users.noreply.github.com>
2020-08-06 13:24:23 +0900

    Update HoT.java
    HoT: Initial revision

--- src/HoT.java
+++ src/HoT.java
@@ -1,2 +1,32 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class HoT {
-}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Tossing a coin...");
+        int h=0,t=0;
+
+        Random rand = new Random();
+
+        for(int i=1;i<4;i++){
+        int j= rand.nextInt(2);
+
+        if(j==0) {
+        System.out.println("Round " + i + ": Heads");
+        h++;
+
+        }
+        else{
+        System.out.println("Round " + i + ": Tails");
+        t++;
+        }
+        }
+
+        System.out.println("Heads: "+h+", Tails: "+t);
+
+        scanner.close();
+        }
+        }
